f7654d5cce01619be0e2f18e6a36bbcc83f96a08
vishwas-yadav-cell <vishwasyadav164@gmail.com>
2020-10-05 16:52:51 +0530

    'initial'

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+
 // Database connection:
 const connectDB = require('./config/db');
 connectDB(); //running the connectDB function here.
+
+// cors handling:
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions));
 
 // Setup Template engine:
 app.set('views', path.join(__dirname, '/views'));
@@ -17,7 +25,7 @@ app.use(express.json());
 // Setting Routes:
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
-app.use('/files/download',require('./routes/download'));
+app.use('/files/download', require('./routes/download'));
 
 const PORT = process.env.PORT || 3000;
 
